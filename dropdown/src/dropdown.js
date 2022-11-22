8ed7c1046dd9d9b403d89f56cf8b7adcb1d74f75
Samir Kaushik <samirkaushik2002@gmail.com>
2022-11-22 20:16:26 +0530

    handleclear function

--- dropdown/src/dropdown.js
+++ dropdown/src/dropdown.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 const Dropdown = () => {
-    const colors = ["cyan", "green", "grey"];
+    const colors = ["cyan", "green", "grey", "steelblue"];
     const [options, setOptions] = useState(colors);
     const [addedOptions, setAddedOptions] = useState([]);
     const handleOptions = (colour) => {
@@ -12,6 +12,10 @@ const Dropdown = () => {
         const newOptions = addedOptions.filter((option) => option !== colour)
         setAddedOptions(newOptions);
         setOptions([...options, colour]);
+    }
+    const handleClear=()=>{
+        setAddedOptions([]);
+        setOptions(colors);
     }
     const clearSVG = <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" viewBox="0 0 16 14">
         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
@@ -33,7 +37,7 @@ const Dropdown = () => {
                     })}
                 </div>
                 <div id="func">
-                    <button id='clear' className="button hoverable">
+                    <button id='clear' className="button hoverable" onClick={handleClear}>
                         {clearSVG}
                     </button>
                     <button id='toggle' className="button hoverable">
